88618b050baaa8d052b8be5ebb2724633c732bc2
tucakadna <tucakadna@gmail.com>
2023-05-09 12:55:16 +0200

    Merge main

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,25 +1,122 @@
-interface TableProps {
-  columns: any;
-  rowData: any;
-  mapRowsUI: any;
-}
+import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
+import React, { useState } from "react";
 
-const Table = ({ columns, rowData, mapRowsUI }: TableProps) => {
-  return (
-    <div className="flex flex-col mt-10">
-      <div className="flex border-b border-b-orange-500">
-        {columns.map((col: { id: string; title: string }) => (
-          <div
-            className="block mb-2 text-sm font-medium text-orange-500 dark:text-white min-w-[200px]"
-            key={col.id}
-          >
-            {col.title}
-          </div>
-        ))}
-      </div>
-      {mapRowsUI(rowData)}
-    </div>
-  );
+type TableRowProps<T> = {
+  item: T;
+  columns: {
+    title: string;
+    key: keyof T;
+  }[];
+  expanded: boolean;
+  onRowClick: () => void;
 };
 
-export default Table;
+function TableRow<T>({
+  item,
+  columns,
+  expanded,
+  onRowClick,
+}: TableRowProps<T>) {
+  return (
+    <>
+      <tr className={expanded ? "bg-gray-100" : ""} onClick={onRowClick}>
+        {columns.map((column) => (
+          <td
+            key={`${column.key as string}-${item[column.key as keyof T]}`}
+            className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3"
+          >
+            {item[column.key] as string}
+          </td>
+        ))}
+        {expanded !== undefined && (
+          <div className="flex items-center justify-center py-4 cursor-pointer">
+            {expanded ? (
+              <MinusIcon className="w-5 h-5" onClick={onRowClick} />
+            ) : (
+              <PlusIcon className="w-5 h-5" onClick={onRowClick} />
+            )}
+          </div>
+        )}
+      </tr>
+      {expanded && (
+        <tr>
+          <td colSpan={columns.length + 1}>
+            <div className="flex flex-col py-2 border-b border-b-gray-200 px-2">
+              <p className="text-sm font-medium text-orange-500">
+                Detailed description:{" "}
+              </p>
+              <p className="text-sm text-gray-600 min-w-[150px] flex items-center">
+                {item.description}
+              </p>
+            </div>
+          </td>
+        </tr>
+      )}
+    </>
+  );
+}
+
+type TableProps<T> = {
+  data: T[];
+  columns: {
+    title: string;
+    key: keyof T;
+  }[];
+};
+
+export default function Table<T>({ data, columns }: TableProps<T>) {
+  const [expandedRows, setExpandedRows] = useState<number[]>([]);
+
+  const handleRowClick = (index: number) => {
+    if (expandedRows.includes(index)) {
+      setExpandedRows(expandedRows.filter((rowIndex) => rowIndex !== index));
+    } else {
+      setExpandedRows([...expandedRows, index]);
+    }
+  };
+
+  if (!data.length) {
+    return (
+      <p className="pt-10 w-full text-center">
+        No items found. Please add a new item.
+      </p>
+    );
+  }
+
+  return (
+    <div>
+      <div className="mt-8 flow-root border border-gray-100 rounded-xl overflow-hidden">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  {columns.map((column) => (
+                    <th
+                      key={column.key.toString()}
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
+                    >
+                      {column.title}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                {data.map((item: T, index: number) => (
+                  <TableRow
+                    key={index}
+                    item={item}
+                    columns={columns}
+                    expanded={expandedRows.includes(index)}
+                    onRowClick={() => handleRowClick(index)}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
